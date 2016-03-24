4316dfd9c4569110959b3526b1ba69561cc3d55d
Huy Tran <huytr402@student.liu.se>
2016-03-24 12:33:37 +0100

    [backend] add morgan for logging

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import schema from './data/schema';
+import morgan from 'morgan';
 import GraphQLHTTP from 'express-graphql';
 
 let app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.static('public'));
 
@@ -10,5 +13,6 @@ app.use('/graphql', GraphQLHTTP({
   schema: schema,
   graphiql: true
 }))
+
 
 app.listen(3000, () => console.log('Listening on port 3000'));
